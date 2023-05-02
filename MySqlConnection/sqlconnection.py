32552a3304dd4d80b90a6c239548e761e254e0ee
Vít Vodička <drakovars1@seznam.cz>
2023-05-02 16:00:41 +0200

    cursor is not dict but when I return database, it just works, so it will probably work with insert too

--- MySqlConnection/sqlconnection.py
+++ MySqlConnection/sqlconnection.py
@@ -1,4 +1,4 @@
-from aiomysql import connect
+from aiomysql import connect,cursors
 
 async def connectToDatabase():
 
@@ -10,6 +10,33 @@ async def connectToDatabase():
             password='ee570b81',
             db='heroku_1cba10abdc691b6'
         )
+
         return await conn
+    except Exception as e:
+        print(e)
+
+
+async def connectToDatabase2():
+    try:
+        conn = await connect(
+            host='eu-cdbr-west-03.cleardb.net',
+            port=3306,
+            user='b851f9ca828e56',
+            password='ee570b81',
+            db='heroku_1cba10abdc691b6'
+        )
+        cursor = await conn.cursor()
+
+        # execute sql query
+        await cursor.execute("SELECT * FROM heroku_1cba10abdc691b6.users WHERE (IdUser=0)")
+
+        # fetch all results
+        r = await cursor.fetchone()
+        # detach cursor from connection
+        await cursor.close()
+
+        # close connection
+        conn.close()
+        return r
     except Exception as e:
         print(e)
--- userParametrs/UserParametrs.py
+++ userParametrs/UserParametrs.py
@@ -9,9 +9,12 @@ class UserParametrs:
             return e
 
     async def getUser(self, id: int):
-        if isinstance(id, int):
+        parameters =sqlconnection.connectToDatabase2()
+        return parameters
+
+        """if isinstance(id, int):
             try:
-                cursor = await self.conn.cursor()
+
                 async with self.conn.cursor() as cur:
                     await cur.execute(f"SELECT * FROM heroku_1cba10abdc691b6.users WHERE (IdUser={id})")
                     await self.conn.commit()
@@ -20,6 +23,6 @@ class UserParametrs:
                 return {"Error": str(e)}
         else:
             return {"Error": "id is not an integer"}
-         #implement here a User
+         #implement here a User"""
     async def getUserParamaetrs(self,id,parametr):
         return #paramaetr and TODO
