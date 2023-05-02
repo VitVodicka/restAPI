7557e89d48efbc92a916ba9b921243da31f14a8f
Vít Vodička <drakovars1@seznam.cz>
2023-05-02 15:40:20 +0200

    now it gets to getUser()

--- main.py
+++ main.py
@@ -16,12 +16,16 @@ async def connect():
 async def createUser(name,surname):
     return await UserFunctions.UsersFunctions.createUser(name,surname)
 
-@app.get("/v1/users/user/getUser/")#missing the numbers after getUSer
-async def getUser():
-    return await UserParametrs.UserParametrs.getUser(id)
-@app.get("/v1/users/user/getUserParametr/")#missing the numbers after getUserParametr and missing parametr
+@app.get("/v1/users/user/getUser/{id}")
+async def getUser(id:int):
+    user = UserParametrs.UserParametrs()
+    return await user.getUser(id)
+
+    """
+    
+/*@app.get("/v1/users/user/getUserParametr/")#missing the numbers after getUserParametr and missing parametr
 async def getUserParametr():
-    return await UserParametrs.UserParametrs.getUserParamaetrs(id,parametr)
+    return await UserParametrs.UserParametrs.getUse(id,parametr)
 @app.get("/v1/users/user/getMessages/")#missing parametr
 async def getMessages():
     return await Messages.Message.getMessage(idSender)
@@ -33,7 +37,8 @@ async def getAllUsers():
     return await UserFunctions.UsersFunctions.getAllUsers()
 
 
+"""
 if __name__ == "__main__":
-    #loop = get_event_loop()
-    #loop.run_until_complete(connectToDatabase())
+    loop = get_event_loop()
+    loop.run_until_complete(connect())
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- userParametrs/UserParametrs.py
+++ userParametrs/UserParametrs.py
@@ -1,8 +1,25 @@
 from MySqlConnection import sqlconnection
 class UserParametrs:
     def __init__(self):
-        self.conn = sqlconnection.connectToDatabase()
-    async def getUser(self,id):
-        return #implement here a User
+
+        try:
+            self.conn = sqlconnection.connectToDatabase()
+
+        except Exception as e:
+            return e
+
+    async def getUser(self, id: int):
+        if isinstance(id, int):
+            try:
+                cursor = await self.conn.cursor()
+                async with self.conn.cursor() as cur:
+                    await cur.execute(f"SELECT * FROM heroku_1cba10abdc691b6.users WHERE (IdUser={id})")
+                    await self.conn.commit()
+                    return {"Working"}
+            except Exception as e:
+                return {"Error": str(e)}
+        else:
+            return {"Error": "id is not an integer"}
+         #implement here a User
     async def getUserParamaetrs(self,id,parametr):
         return #paramaetr and TODO
